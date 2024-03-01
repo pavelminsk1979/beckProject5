2030cc09117eea83139a43e757dd754530828b17
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-03-02 02:10:28 +0300

    creat auth/login andpoind..

--- src/repositories/users/user-query-repository.ts
+++ src/repositories/users/user-query-repository.ts
@@ -1,4 +1,4 @@
-import { usersCollection} from "../../db/mongoDb";
+import {usersCollection} from "../../db/mongoDb";
 import {ObjectId} from "mongodb";
 import {userMaper} from "../../mapers/userMaper";
 import {OutputUser, PaginationWithOutputUser, QueryUsersInputModal} from "../../allTypes/userTypes";
@@ -7,44 +7,50 @@ import {queryParamsValidationUsers} from "../../middlewares/usersMiddlewares/que
 
 export const userQueryRepository = {
 
-    async findUserByLoginOrEmail(loginOrEmail:string){
+    async findUserByLoginOrEmail(loginOrEmail: string) {
 
-        const user = await usersCollection.findOne({$or:[{login:loginOrEmail},{email:loginOrEmail}]})
+        const user = await usersCollection.findOne({$or: [{login: loginOrEmail}, {email: loginOrEmail}]})
         return user
     },
 
 
-    async findUserById(id: string):Promise<OutputUser|null> {
+    async findUserById(id: string): Promise<OutputUser | null> {
         const user = await usersCollection.findOne({_id: new ObjectId(id)})
         if (!user) return null
 
-            return userMaper(user)
+        return userMaper(user)
     },
 
 
-    async getUsers(queryParams:QueryUsersInputModal):Promise<PaginationWithOutputUser<OutputUser>>{
+    async getUsers(queryParams: QueryUsersInputModal): Promise<PaginationWithOutputUser<OutputUser>> {
 
-       const {pageNumber,pageSize,sortBy,sortDirection,searchLoginTerm,searchEmailTerm}=queryParamsValidationUsers(queryParams)
+        const {
+            pageNumber,
+            pageSize,
+            sortBy,
+            sortDirection,
+            searchLoginTerm,
+            searchEmailTerm
+        } = queryParamsValidationUsers(queryParams)
 
-        let filter = {}
-        if (searchLoginTerm) {
-            filter = {
-                login: {
-                    $regex: searchLoginTerm,
-                    $options: 'i'
-                }
-            }
+
+        let filter: {/*it's type*/ $or: object[] }  = {$or: []}
+
+        if(searchLoginTerm){
+            filter.$or.push({
+                login:{  $regex: searchLoginTerm,
+                    $options: 'i'}
+            })
         }
-        if (searchEmailTerm) {
-            filter = {
-                ...filter,
+
+        if(searchEmailTerm){
+            filter.$or.push({
                 email: {
                     $regex: searchEmailTerm,
                     $options: 'i',
-                },
-            };
+                }
+            })
         }
-
 
         const users = await usersCollection
             .find(filter)
@@ -65,8 +71,6 @@ export const userQueryRepository = {
             items: users.map(userMaper)
         }
     },
-
-
 
 
 }
