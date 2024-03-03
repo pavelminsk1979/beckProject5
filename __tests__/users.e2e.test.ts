05363af665c2e16d986d1beb6ef22a17267de3d7
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-03-03 22:36:28 +0300

    create tests users.

--- __tests__/users.e2e.test.ts
+++ __tests__/users.e2e.test.ts
@@ -14,8 +14,6 @@ describe('/users',()=>{
 
 
     let idNewUser1:string
-    let idNewLogin:string
-    let idNewEmail:string
     let idNewUser2:string
 
 
@@ -26,7 +24,7 @@ describe('/users',()=>{
         const res2=await req
             .post('/users')
             .set('Authorization', `Basic ${loginPasswordBasic64}`)
-            .send({ login: 'login123',
+            .send({ login: 'log123',
                 password: '11111111',
                 email:'pavPav@mail.ru'})
 
@@ -43,11 +41,9 @@ describe('/users',()=>{
             .expect(STATUS_CODE.CREATED_201)
 
         idNewUser1=res1.body.id
-        idNewLogin=res1.body.login
-        idNewEmail=res1.body.email
 
-        expect(res1.body.login).toEqual(idNewLogin)
-        expect(res1.body.email).toEqual(idNewEmail)
+        expect(res1.body.login).toEqual('log111')
+        expect(res1.body.email).toEqual('pavelPavel@mail.ru')
         expect(res1.body.id).toEqual(idNewUser1)
 
 
@@ -64,8 +60,65 @@ describe('/users',()=>{
         //console.log(res.body.items)
 
             expect(res.body.items[0].id).toEqual(idNewUser1)
-            expect(res.body.items[0].login).toEqual(idNewLogin)
-            expect(res.body.items[0].email).toEqual(idNewEmail)
+            expect(res.body.items[1].id).toEqual(idNewUser2)
+            expect(res.body.items[0].login).toEqual('log111')
+            expect(res.body.items[1].login).toEqual('log123')
+    })
+
+
+
+
+    // query params(sortBy,sortDirection,pageNumber,pageSize,searchLoginTerm,searchEmailTerm)
+
+    it('get users-- query params sortDirection DESC',async ()=>{
+        const res = await req
+            .get('/users?sortBy=login&sortDirection=desc')
+            .set('Authorization', `Basic ${loginPasswordBasic64}`)
+            .expect(STATUS_CODE.SUCCESS_200)
+
+        //console.log(res.body.items)
+
+        expect(res.body.items[0].login).toEqual( 'log123' )
+        expect(res.body.items[1].login).toEqual( 'log111' )
+
+    })
+
+
+    it('get users-- query params sortDirection ASC',async ()=>{
+        const res = await req
+            .get('/users?sortBy=login&sortDirection=asc')
+            .set('Authorization', `Basic ${loginPasswordBasic64}`)
+            .expect(STATUS_CODE.SUCCESS_200)
+
+        //console.log(res.body.items)
+
+        expect(res.body.items[0].login).toEqual( 'log111' )
+        expect(res.body.items[1].login).toEqual( 'log123' )
+
+    })
+
+
+    it('get users-- query params searchLoginTerm',async ()=>{
+        const res = await req
+            .get('/users?searchLoginTerm=og1')
+            .set('Authorization', `Basic ${loginPasswordBasic64}`)
+            .expect(STATUS_CODE.SUCCESS_200)
+
+        //console.log(res.body.items)
+
+        expect(res.body.items.length).toEqual( 2 )
+
+    })
+
+    it('get users-- query params searchLoginTerm',async ()=>{
+        const res = await req
+            .get('/users?searchLoginTerm=og12')
+            .set('Authorization', `Basic ${loginPasswordBasic64}`)
+            .expect(STATUS_CODE.SUCCESS_200)
+
+        //console.log(res.body.items)
+
+        expect(res.body.items.length).toEqual( 1 )
 
     })
 
